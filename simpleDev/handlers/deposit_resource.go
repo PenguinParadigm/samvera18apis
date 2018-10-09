fe755e76693eb660f6a6e37a685527ffe64dae8a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2018-10-09 11:55:48 -0600

    Updating documentation for clarification
    Capturing some of the narrative that Justin spoke about during
    his presentation at Samvera Connect 2018

--- simpleDev/handlers/deposit_resource.go
+++ simpleDev/handlers/deposit_resource.go
@@ -20,11 +20,14 @@ type depositResource struct{}
 // Handle the create resource request
 func (d *depositResource) Handle(params operations.DepositResourceParams, agent *models.Agent) middleware.Responder {
 	// Grab the data passed in from the call
+	// The params is an `operations.DepositResourceParams`, which is defined in the
+	// `generated/restapi/operations/deposit_resource_parameters.go` file
 	resource := params.Payload
 
 	// Mint an identifier (UUID)
 	uuid, err := uuid.NewRandom()
 	if err != nil {
+		// Explode. I don't know what to do if you can't generate a random number
 		panic(err)
 	}
 
@@ -33,6 +36,7 @@ func (d *depositResource) Handle(params operations.DepositResourceParams, agent 
 	identifier = strfmt.UUID(uuid.String())
 	resource.Identifier = identifier
 	resource.Depositor = agent
+	// Do a declaration ahead of time; instead of `var version int64; version = int64(1)`, we could do `version := int64(1)
 	var version int64
 	version = int64(1)
 	resource.Version = version
